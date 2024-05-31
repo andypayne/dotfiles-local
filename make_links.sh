dfc751f460f97119f3373414d6d598364dab76d1
andypayne <apayne@gmail.com>
2024-05-31 06:35:54 -0700

    Add make_links script

---
+++ make_links.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ln -s ~/dotfiles-local/zshrc.local ~/.zshrc.local
+ln -s ~/dotfiles-local/aliases.local ~/.aliases.local
+ln -s ~/dotfiles-local/vimrc.local ~/.vimrc.local
+ln -s ~/dotfiles-local/gitconfig.local ~/.gitconfig.local
+ln -s ~/dotfiles-local/tmux.conf.local ~/.tmux.conf.local
+ln -s ~/dotfiles-local/alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -s ~/dotfiles-local/nvim/init.vim ~/.config/nvim/init.vim
+ln -s ~/dotfiles-local/kitty/kitty.conf ~/.config/kitty/kitty.conf
+
